2db624334ed3b74bd15422f10b1e7c738aa3da2d
KsRyY <andy200511@126.com>
2019-08-30 11:33:22 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# rbenv-china-mirror
+
+此项目是[rbenv-china-mirror](https://github.com/AndorChen/rbenv-china-mirror)的`pyenv`版本。
+
+这是一个 [pyenv](https://github.com/pyenv/pyenv) 插件，作用是使用 [淘宝的Python镜像](https://npm.taobao.org/mirrors/python/)安装 Python.
+
+## 安装方法
+
+在命令行中执行下述命令：
+
+```sh
+$ git clone https://github.com/KsRyY/pyenv-china-mirror.git "$(pyenv root)"/plugins/pyenv-china-mirror
+```
+
+## 使用方法
+
+像往常一样安装 Python 即可，无需额外设置。
+
+```sh
+$ py install 3.7.4
+```
+
+## 升级方法
+
+进入 `pyenv-china-mirror` 插件所在的目录，拉取这个仓库里的最新代码：
+
+```
+$ cd ~/.pyenv/plugins/pyenv-china-mirror
+$ git pull
+```
+
+## 卸载方法
+
+进入 `~/.pyenv/plugins/` 目录，把 `pyenv-china-mirror` 文件夹整个删除即可。
+
+## 工作原理
+
+安装前，替换 Python 安装定义文件中 Python 的下载地址。安装后，再把地址改为原值。
+
+## 作者
+
+原作者：[Andor](http://about.ac)
+修改者：[KsRyY](https://konomo.now.sh)
+
+## 许可证
+
+MIT
---
+++ etc/pyenv.d/install/china-mirror.bash
@@ -0,0 +1,44 @@
+if declare -Ff before_install >/dev/null; then
+  before_install change_to
+  after_install change_back
+else
+  echo "pyenv: no hooks for before_install, consider update your pyenv version" >&2
+fi
+
+DEFINITION_PATH="${ARGUMENTS[0]}"
+
+change_to() {
+  # skip build-in mirror
+  export PYTHON_BUILD_SKIP_MIRROR=1
+
+  if [ -z "$DEFINITION_PATH" ]; then
+    usage 1 >&2
+  elif [ ! -f "$DEFINITION_PATH" ]; then
+
+    PYTHON_BUILD_INSTALL_PREFIX="$(pyenv root)/plugins/python-build"
+
+    OLDIFS="$IFS"
+    IFS=: PYTHON_BUILD_DEFINITIONS=($PYTHON_BUILD_DEFINITIONS ${PYTHON_BUILD_ROOT:-$PYTHON_BUILD_INSTALL_PREFIX}/share/python-build)
+
+    for DEFINITION_DIR in "${PYTHON_BUILD_DEFINITIONS[@]}"; do
+      if [ -f "${DEFINITION_DIR}/${DEFINITION_PATH}" ]; then
+        DEFINITION_PATH="${DEFINITION_DIR}/${DEFINITION_PATH}"
+        perl -p -i -e 's!www.python.org/ftp!npm.taobao.org/mirrors!' ${DEFINITION_PATH}
+        break
+      fi
+    done
+
+    IFS="$OLDIFS"
+
+    if [ ! -f "$DEFINITION_PATH" ]; then
+      echo "python-build: definition not found: ${DEFINITION_PATH}" >&2
+      exit 2
+    fi
+  fi
+}
+
+change_back() {
+  if [ ! -f "${ARGUMENTS[0]}" ]; then
+    perl -p -i -e 's!npm.taobao.org/mirrors!www.python.org/ftp!' ${DEFINITION_PATH}
+  fi
+}
